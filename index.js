d14cf407db12447f58837580145b5d9b2ecca05f
HadIhab <chocovani101@hotmail.fr>
2021-03-20 19:15:57 +0100

    First Initialisation

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var ingredients = [ {id:0, text:'Pepper'},{id:1, text:'Salt'},{id:2, text:'Sugar'} ];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',function(req,res){
+	res.send('This is the base URL of this simple API.');
+});
+app.get('/ingredients',function(req,res){
+	res.send(ingredients);
+});
+app.delete('/ingredients/:ingredientId',function(req,res){
+	
+});
+app.put('/ingredients/:ingredientId',function(req,res){
+
+	var newText = req.body.text;
+	if (!newText || newText === ""){
+		res.status(500).send({error: "You must provide ingredient text"})
+	}
+	else{
+		for (var i = 0; i < ingredients.length; i++ ){
+			
+			if(ingredients[i].id == req.params.ingredientId){
+				ingredients[i].text = newText;
+				break;
+			}
+		}
+		res.send(ingredients);
+	}
+});
+
+app.listen(3000,function(){
+	console.log('[INFO]: CRUD API is running on server..');
+});
